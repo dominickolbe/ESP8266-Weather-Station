cbf645669b354c087647908075333da414ae56c4
Dominic Kolbe <hi@dominickolbe.dk>
2018-04-22 15:31:26 +0200

    added ssd1306 oled display

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ESP8266-Weather-Station
 Weather Station based on ESP8266 - DHT22, LDR, LM35 and SSD1306
+
+## Hardware
+* ESP8266 NodeMCU
+* SSD1306 OLED
+* DHT22
+* LDR
+
+## Project setup
+
+### IDE
+
+I'm using `Visual Studio Code` and the `PlatformIO IDE` Plugin.
+
+### Modifying the SSD1306 Driver
+
+uncomment the line, that fits your display
+
+`.piolibdeps/Adafruit SSD1306_ID135/Adafruit_SSD1306.h`
+```
+#define SSD1306_128_64
+// #define SSD1306_128_32
+// #define SSD1306_96_16
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
+
+Adafruit_SSD1306 display(16); // D0
+
+void turnDisplay(bool state) {
+  if (state) {
+    display.ssd1306_command(SSD1306_DISPLAYON);
+  } else {
+    display.ssd1306_command(SSD1306_DISPLAYOFF);
+  }
+}
+
+void splashscreen() {
+  display.clearDisplay();
+
+  display.setTextColor(WHITE);
+  display.setTextSize(2);
+
+  display.setCursor(22, 15);
+  display.println("Weather");
+
+  display.setCursor(22, 40);
+  display.println("Station");
+
+  display.display();
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("");
+
+  Serial.println("START: setup()");
+
+  // init display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  Serial.println("splashscreen()");
+  splashscreen();
+
+  Serial.println("END: setup()");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
